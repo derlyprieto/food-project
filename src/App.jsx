a3235086d9ba0a5703758da46be048beb6505cfc
Derly Prieto <derly@MacBook-Pro-de-Derly.local>
2023-03-13 22:58:50 -0500

    Pedido

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import PedidoPizza from './Components/PedidoPizza'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <PedidoPizza/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/PedidoPizza.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+
+const PedidoPizza = () => {
+    const [item, setItem] = useState('')
+  
+
+    useEffect(() => {
+       console.log('Se montó el componente de función')
+       setTimeout(() => {
+            setItem('Pizza')
+       }, 2000)
+       return () => {
+           console.log('Se desmontó el componente de función')
+       }
+    }, [])
+
+
+    useEffect(() => {
+      console.log('Se actualizó count dentro del componente de función')
+  }, [name])
+    
+    console.log('Se renderizó el componente de función')
+
+  return (
+    <div>
+        <h1>Su Pedido:</h1>
+        <h3>{item}</h3>
+        <button onClick={()=> setItem('')}>Cancelar pedido</button>
+    </div>
+  )
+}
+
+export default PedidoPizza
